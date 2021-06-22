e1443a892144d3ca7605b3d0e7adb1c27f778e51
kirill <asdas>
2021-06-22 16:23:04 +0300

    init

---
+++ src/components/Test.jsx
@@ -0,0 +1,11 @@
+function Test(){
+    return (
+        <>
+            <div className="menu">
+                <div className="menu__link">1</div>
+            </div>
+        </>
+    );
+}
+
+export default Test;
