34ff099f8554dad3f5365614e285677516ef8659
Olnovikov <oleg-novikov26@yandex.ru>
2021-06-23 23:10:29 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Test from "./components/Test";
+import One_more_test from "./components/One_more_test";
 import "./scss/index.scss";
 
 function App() {
   return (
     <div className="App">
-        <Test />
+      <Test />
+      <One_more_test />
     </div>
+
+
+
   );
 }
 
