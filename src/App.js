a9674b39184240bbba75d0fdac8dc32c8044d618
kirill <asdas>
2021-06-23 23:18:26 +0300

    add background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import {Footer} from "./components/Footer";
+import {News} from "./components/news";
 import "./scss/index.scss";
 
 function App() {
   return (
     <>
+      <News />
       <Footer />
     </>
   );
---
+++ src/components/news/index.jsx
@@ -0,0 +1,11 @@
+export function News(){
+    return (
+        <section className="news">
+            <div className="container">
+                <h2 className="text-red heading">
+                    Новости
+                </h2>
+            </div>
+        </section>
+    );
+}
